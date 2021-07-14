a92d433ffbd4ec83b0a9c1c0b7fbb01512784c60
isrt09 <mazedur@gmail.com>
2021-07-14 23:46:33 +0600

    Post, Category, Author added

--- app/models.py
+++ app/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+User = get_user_model()
+
+class Author(models.Model):
+	user    = models.OneToOneField(User, on_delete=models.CASCADE)
+	photo   = models.ImageField()
+
+	def __str__(self):
+		return self.user.username
+
+class Category(models.Model):
+	title  = models.CharField(max_length=20)
+
+	def __str__(self):
+		return self.title
+
+class Post(models.Model):
+	title     	   = models.CharField(max_length=100)
+	content   	   = models.TextField()
+	timestamp 	   = models.DateField(auto_now_add=True)
+	comment_counts = models.IntegerField(default=0)
+	thumnail       = models.ImageField()
+	author		   = models.ForeignKey(Author,        on_delete=models.CASCADE)
+	category	   = models.ManyToManyField(Category)
+	featured 	   = models.BooleanField()
+
+	def __str__(self):
+		return self.title
