033d169a587d1356cbdee4e9d6c4c683de11dea3
isrt09 <mazedur@gmail.com>
2021-07-18 23:04:59 +0600

    Model Modified

--- app/models.py
+++ app/models.py
@@ -1,32 +1,38 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from tinymce import HTMLField
 
 # Create your models here.
 
 User = get_user_model()
 
 class Author(models.Model):
-	user    = models.OneToOneField(User, on_delete=models.CASCADE)
-	photo   = models.ImageField()
+	user    = models.OneToOneField(User,on_delete=models.CASCADE)
+	profile = models.ImageField()
 
 	def __str__(self):
 		return self.user.username
 
 class Category(models.Model):
-	title  = models.CharField(max_length=20)
+	title      = models.CharField(max_length=100)
 
 	def __str__(self):
 		return self.title
 
+	class Meta:
+		verbose_name  		= 'Category'
+		verbose_name_plural = 'Categories'
+
 class Post(models.Model):
-	title     	   = models.CharField(max_length=100)
-	content   	   = models.TextField()
-	timestamp 	   = models.DateField(auto_now_add=True)
-	comment_counts = models.IntegerField(default=0)
-	thumnail       = models.ImageField()
-	author		   = models.ForeignKey(Author,        on_delete=models.CASCADE)
-	category	   = models.ManyToManyField(Category)
-	featured 	   = models.BooleanField()
+	title      = models.CharField(max_length=100)
+	content    = HTMLField('Content') # content    = models.TextField()	
+	timestamp  = models.DateTimeField(auto_now_add=True)
+	views_count= models.IntegerField()
+	comments   = models.IntegerField(default=0)
+	thumbnail  = models.ImageField()
+	author     = models.ForeignKey(Author, on_delete=models.CASCADE)
+	categories = models.ManyToManyField(Category)
+	featured   = models.BooleanField()
 
 	def __str__(self):
 		return self.title
