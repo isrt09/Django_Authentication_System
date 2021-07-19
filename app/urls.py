b685ac54cfc5ca2d387148a4e88bac474efa9905
isrt09 <mazedur@gmail.com>
2021-07-19 23:53:13 +0600

    Home added

--- app/urls.py
+++ app/urls.py
@@ -4,5 +4,6 @@ from app import views
 urlpatterns = [    
     path('',          views.home,  name = 'home'),
     path('login/',    views.login, name = 'login'),
-    path('register/', views.login, name = 'register')
+    path('register/', views.login, name = 'register'),
+    path('home/',     views.home,  name = 'home')
 ]
